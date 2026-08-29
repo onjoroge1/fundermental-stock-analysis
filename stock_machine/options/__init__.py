51bb3bc00f2855e4315dffa2342554e02b704689
onjoroge1 <kim.njo@gmail.com>
2026-08-29 14:06:04 -0400

    Expose extended option strategy valuation

--- stock_machine/options/__init__.py
+++ stock_machine/options/__init__.py
@@ -1,4 +1,4 @@
-"""Read-only option payoff, filtering, and strategy generation."""
+"""Read-only option payoff, filtering, strategy generation and valuation."""
 
 from .generator import GenerationPolicy, generate_strategies
 from .forecast_io import load_latest_forecast
@@ -9,18 +9,28 @@ from .models import (
     StrategyGenerationResult,
     StrategyType,
 )
+from .extended import (
+    ExtendedPolicy,
+    ExtendedStrategyType,
+    covered_call,
+    mixed_expiration,
+)
 from .payoff import expiration_pnl, payoff_points, summarize_payoff
 
 __all__ = [
+    "ExtendedPolicy",
+    "ExtendedStrategyType",
     "GenerationPolicy",
     "OptionAction",
     "OptionLeg",
     "StrategyCandidate",
     "StrategyGenerationResult",
     "StrategyType",
+    "covered_call",
     "expiration_pnl",
     "generate_strategies",
     "load_latest_forecast",
+    "mixed_expiration",
     "payoff_points",
     "summarize_payoff",
 ]
